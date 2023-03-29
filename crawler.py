d373f4cfe58579018abc643ef246007b138ace73
Narsi2Pydev <57086727+Narsi2Pydev@users.noreply.github.com>
2023-03-29 22:00:52 +0530

    Update crawler.py
    Updated with try catch

--- crawler.py
+++ crawler.py
@@ -12,45 +12,48 @@ crawled_list = []
 crawled_list_dict = []
 
 def crawl_page(url, depth):
+    try:
+        # Send a GET request to the URL
+        response = requests.get(url)
 
-    # Send a GET request to the URL
-    response = requests.get(url)
+        # Parse the HTML content of the page using BeautifulSoup
+        soup = BeautifulSoup(response.content, 'html.parser')
+        max_depth = 1 #default
+        # Extract all the links and images on the page
+        links = soup.find_all('a')
+        if depth == 0 or depth == 1:
+            max_depth = 1
+        else:
+            max_depth = depth
+        #links.insert(0, url)
+        for index, link in enumerate(links):
+            if index == max_depth:
+                return crawled_list_dict
+            # Get the href attribute of each link
+            href = link.get('href')
+            # If the link is relative, add the domain name to it
+            if href.startswith('/'):
+                href = url + href
 
-    # Parse the HTML content of the page using BeautifulSoup
-    soup = BeautifulSoup(response.content, 'html.parser')
-    max_depth = 1 #default
-    # Extract all the links and images on the page
-    links = soup.find_all('a')
-    if depth == 0 or depth == 1:
-        max_depth = 1
-    else:
-        max_depth = depth
-    links.insert(0, url)
-    for index, link in enumerate(links):
-        if index == max_depth:
-            return crawled_list_dict
-        # Get the href attribute of each link
-        href = link.get('href')
-        # If the link is relative, add the domain name to it
-        if href.startswith('/'):
-            href = url + href
+            if href not in crawled_list:
+                response_href = requests.get(href)
+                soup_href = BeautifulSoup(response_href.content, 'html.parser')
+                images = soup_href.find_all('img', {'src': re.compile('.jpg|.jpeg|.png|.gif|.bmp', re.IGNORECASE)})
 
-        if href not in crawled_list:
-            response_href = requests.get(href)
-            soup_href = BeautifulSoup(response_href.content, 'html.parser')
-            images = soup_href.find_all('img', {'src': re.compile('.jpg|.jpeg|.png|.gif|.bmp', re.IGNORECASE)})
+                # Add the images to the crawled dictionary with their source URL
+                for image in images:
+                    src = image['src']
+                    if src.startswith('//'):
+                        src = 'https:' + src
+                    crawled['imageUrl'] = src
+                    crawled['sourceUrl'] = href
+                    crawled['depth'] = depth
 
-            # Add the images to the crawled dictionary with their source URL
-            for image in images:
-                src = image['src']
-                if src.startswith('//'):
-                    src = 'https:' + src
-                crawled['imageUrl'] = src
-                crawled['sourceUrl'] = href
-                crawled['depth'] = depth
-
-                crawled_list_dict.append(crawled)
-    return crawled_list_dict
+                    crawled_list_dict.append(crawled)
+        return crawled_list_dict
+    except Exception as exe:
+        print(str(exe))
+        return 'No Image Found'
 
 
 if __name__ == "__main__":
@@ -58,7 +61,7 @@ if __name__ == "__main__":
     url = 'http://store.steampowered.com/tags/en-us/RPG/'  #str(input()) #specify the url and test
     depth = 1 #int(input())
     output = crawl_page(url, depth)
-
-    # Save the crawled URLs to a JSON file
-    with open('request.json', 'w') as file:
-        json.dump({'result': output}, file, indent=4)
+    if output != 'No Image Found':
+        # Save the crawled URLs to a JSON file
+        with open('request.json', 'w') as file:
+            json.dump({'result': output}, file, indent=4)
